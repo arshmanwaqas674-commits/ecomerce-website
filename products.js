7c2cd0f41edde69f04aac29600783432b2fae7c7
arshmanwaqas674-commits <arshmanwaqas674@gmail.com>
2026-07-25 01:04:01 +0500

    Add files via upload

---
+++ products.js
@@ -0,0 +1,213 @@
+
+const products = [
+
+{
+id:1,
+name:"olive green tee",
+category:"T-Shirt",
+price:2499,
+image:"img-1.jpeg"
+},
+
+{
+id:2,
+name:"fully black",
+category:"T-Shirt",
+price:2499,
+image:"img-2.jpeg"
+},
+
+{
+id:3,
+name:"off white",
+category:"T-Shirt",
+price:2399,
+image:"img-3.jpeg"
+},
+
+{
+id:4,
+name:"blast black",
+category:"T-Shirt",
+price:2499,
+image:"img-4.jpeg"
+},
+
+{
+id:5,
+name:"mastered",
+category:"T-Shirt",
+price:2599,
+image:"img-5.jpeg"
+},
+
+{
+id:6,
+name:"dark black",
+category:"T-Shirt",
+price:2499,
+image:"img-6.jpeg"
+},
+
+{
+id:7,
+name:"white",
+category:"T-Shirt",
+price:2599,
+image:"img-7.jpeg"
+},
+
+{
+id:8,
+name:"cream ",
+category:"T-Shirt",
+price:2499,
+image:"img-8.jpeg"
+},
+
+{
+id:9,
+name:"light blue",
+category:"T-Shirt",
+price:2399,
+image:"img-9.jpeg"
+},
+
+{
+id:10,
+name:"jat black",
+category:"T-Shirt",
+price:2499,
+image:"img-10.jpeg"
+},
+
+{
+id:11,
+name:"olive green ",
+category:"Shirt",
+price:2999,
+image:"img-1.jpeg"
+},
+
+{
+id:12,
+name:"light beige.",
+category:"Shirt",
+price:3199,
+image:"img-12.jpeg"
+},
+
+{
+id:13,
+name:"brown",
+category:"Shirt",
+price:2999,
+image:"img-13.jpeg"
+},
+
+{
+id:14,
+name:"common black",
+category:"Shirt",
+price:3099,
+image:"img-14.jpeg"
+},
+
+{
+id:15,
+name:"creamy",
+category:"Shirt",
+price:2899,
+image:"img-15.jpeg"
+},
+
+{
+id:16,
+name:"Matte Black",
+category:"Shirt",
+price:2999,
+image:"img-16.jpeg"
+},
+
+{
+id:17,
+name:"gray",
+category:"Shirt",
+price:3199,
+image:"img-17.jpeg"
+},
+
+{
+id:18,
+name:"Faded Black",
+category:"Shirt",
+price:2999,
+image:"img-18.jpeg"
+},
+
+{
+id:19,
+name:"fully mastered",
+category:"Shirt",
+price:3099,
+image:"img-19.jpeg"
+},
+
+{
+id:20,
+name:"light brown",
+category:"Shirt",
+price:3299,
+image:"img-20.jpeg"
+},
+{
+id:21,
+name:"creamy blue",
+category:"Shirt",
+price:3299,
+image:"img-21.jpeg"
+},
+{
+id:22,
+name:"white",
+category:"Shirt",
+price:3299,
+image:"img-22.jpeg"
+},
+{
+id:23,
+name:"pure white",
+category:"Shirt",
+price:3299,
+image:"img-23.jpeg"
+},
+{
+id:24,
+name:"dark brown",
+category:"Shirt",
+price:3299,
+image:"img-24.jpeg"
+},
+{
+id:25,
+name:"ice blue",
+category:"Shirt",
+price:3299,
+image:"img-25.jpeg"
+},
+{
+id:26,
+name:"black",
+category:"Shirt",
+price:3299,
+image:"img-26.jpeg"
+},
+{
+id:27,
+name:"powder ice",
+category:"Shirt",
+price:3299,
+image:"img-27.jpeg"
+},
+
+];
